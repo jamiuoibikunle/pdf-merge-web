f7f26723c52f27ab9b1b5a432bca51902879668f
Jamiu Ibikunle <ibidotun@gmail.com>
2023-03-28 21:57:53 +0100

    Pegged home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from "react";
+import { Box, Button, HStack, Input } from "@chakra-ui/react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [files, setFiles] = useState([]);
 
-export default Home
+  const handleAddFile = (e: ChangeEvent<HTMLInputElement>) => {
+    setFiles([...files]);
+  };
+
+  const handleNewFile = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.files);
+  };
+
+  return (
+    <Box p={5} w="100%">
+      <HStack>
+        <Input w="lg" type="file" onChange={handleNewFile} />
+        <Button colorScheme="blue">Enter</Button>
+      </HStack>
+    </Box>
+  );
+};
+
+export default Home;
